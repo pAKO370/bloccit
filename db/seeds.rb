8acbea4f3c5849dbedc7f18b7e819eb3bb060e21
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-02 08:56:14 -0600

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,14 @@ posts = Post.all
     )
     end
 
+    puts "#{Post.count}"
+    Post.find_or_create_by(title: "New unique title", body: "Unique Body")
+    puts "#{Post.count}"
+
+    puts "#{Comment.count}"
+    Comment.find_or_create_by(body: "Unique Body")
+    puts "#{Comment.count}"
+
     puts "Seed fininshed"
     puts "#{Post.count} posts created"
     puts "#{Comment.count} comments created"

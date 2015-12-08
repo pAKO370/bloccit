95dc86202149aeffddfd526af2102d6c210dd6d0
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-08 17:19:51 -0600

    labels

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,10 +4,11 @@ include RandomData
 RSpec.describe Post, type: :model do
   
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
- # #4
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
- 
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+
+  it { should have_many(:labelings)}
+  it { should have_many(:labels).through(:labelings)}
 
   it {should have_many(:comments)}
 

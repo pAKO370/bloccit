7f6b908d5981f160fbc833f25407112e736dda32
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-09 10:53:51 -0600

    voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,15 +22,21 @@ include RandomData
  end
  topics = Topic.all
 
-50.times do 
+ 
 
-  Post.create!(
+50.times do
+   
+   post = Post.create!(
+     user:   users.sample,
+     topic:  topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
 
-    user: users.sample,
-    topic:  topics.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-    )
+post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
 
 posts = Post.all
@@ -62,4 +68,5 @@ posts = Post.all
     puts "#{Topic.count} topics were created"
     puts "#{Post.count} posts created"
     puts "#{Comment.count} comments created"
+    puts "#{Vote.count} votes created"
     

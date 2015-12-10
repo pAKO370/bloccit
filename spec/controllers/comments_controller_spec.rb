8d78fdd57569f11eb4d7b488283db3132aa5f9ad
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-10 10:22:03 -0600

    Public profile

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,13 +4,13 @@ include RandomData
  include SessionsHelper
 
  RSpec.describe CommentsController, type: :controller do
-   let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-   let(:other_user) { User.create!(name: RandomData.random_name, email: RandomData.random_email, password: "helloworld", role: :member) }
-   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-   let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
+   let(:my_topic) { create(:topic) }
+   let(:my_user) { create(:user) }
+   let(:other_user) { create(:user) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
    let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
  
- # #6
+ 
    context "guest" do
      describe "POST create" do
        it "redirects the user to the sign in view" do
@@ -37,6 +37,7 @@ include RandomData
        it "increases the number of comments by 1" do
          expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
        end
+
  
        it "redirects to the post show view" do
          post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
@@ -115,25 +116,8 @@ include RandomData
        end
      end
    end
-   describe "after_create" do
- # #22
-     before do
-       @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
-     end
+   
+ end
+
  
- # #23
-     it "sends an email to users who have favorited the post" do
-       favorite = user.favorites.create(post: post)
-       expect(FavoriteMailer).to receive(:new_comment).with(user, post, @another_comment).and_return(double(deliver_now: true))
- 
-       @another_comment.save
-     end
- 
- # #24
-     it "does not send emails to users who haven't favorited the post" do
-       expect(FavoriteMailer).not_to receive(:new_comment)
- 
-       @another_comment.save
-     end
-   end
-end
+

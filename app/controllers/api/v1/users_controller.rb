525343ed51f60e6023e3443c09176b8bde569d7b
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-13 17:17:21 -0600

    API sending data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,46 @@
 class Api::V1::UsersController < Api::V1::BaseController
-   before_filter :authenticate_user
-   before_filter :authorize_user
+  before_filter :authenticate_user
+  before_filter :authorize_user
  
-   def show
-     user = User.find(params[:id])
- # #17
-     render json: user.to_json, status: 200
-   end
+  def show
+    user = User.find(params[:id])
+
+    render json: user.to_json, status: 200
+  end
  
-   def index
-     users = User.all
- # #18
-     render json: users.to_json, status: 200
-   end
- end
+  def index
+    users = User.all
+
+    render json: users.to_json, status: 200
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update_attributes(user_params)
+      render json: user.to_json, status: 200
+    else
+      render json: {error: "User update failed", status: 400},status: 400
+    end
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save!
+      render json: user.to_json, status: 201
+    else
+      render json: {error: "User is invalid", status: 400}, status: 400
+    end
+
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
+
+end
